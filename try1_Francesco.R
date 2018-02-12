b8d23f2829b3abd8948db98c759ea073fd0e5eee
Francesco Ignazio Re <francescoignazio97@gmail.com>
2018-02-12 18:44:16 -0500

    here you find t-test and all the other computational things embedded in the pdf

--- try1_Francesco.R
+++ try1_Francesco.R
@@ -6,6 +6,58 @@ years <- msum[c(1)]
 msum  <- msum[-c(1)]
 rownames(msum) <- as.vector(t(years))
 msum
+##################################################################################################################
+#histogram
+ap10 <- read.csv("BP apprehensions 2010.csv")
+cities <- ap10[,1]
+ap10 <- ap10[-c(1,14)]
+rownames(ap10) <- cities
+SumCol10 <- colSums(Filter(is.numeric, ap10))
+
+ap17 <- read.csv("edited_app17.csv")
+ap17 <- ap17[-c(1,2,15)]
+rownames(ap17) <- cities
+SumCol17 <- colSums(Filter(is.numeric, ap17))
+
+CmbColS <- rbind((SumCol10),(SumCol17))
+CmbRowS <- rbind(rowSums(ap10), rowSums(ap17))
+colours <- c("orange","red")
+
+
+#Comparison total apprehensions by month 
+
+barplot(CmbColS,
+        beside=TRUE, 
+        las=2, 
+        main = "Apprehensions by month",
+        cex.lab =1.5,
+        col = colours )
+legend("topright",
+       c("Apprehensions in 2010","Apprehensions in 2017"),
+       cex=0.8,
+       bty="n",
+       fill=colours )
+
+#Comparison total apprehensions by city
+barplot(CmbRowS,
+        beside=TRUE, 
+        las=2, main = "Apprehensions by city", 
+        cex.lab =1.5, 
+        col = colours )
+legend("topleft",
+       c("Apprehensions in 2010","Apprehensions in 2017"),
+       cex=0.8,
+       bty="n",
+       fill=colours )
+
+
+
+
+
+
+
+##################################################################################################################
+
 
 #monthly apprehensions by Sector
 ap10 <- read.csv("BP apprehensions 2010.csv")
@@ -64,6 +116,8 @@ by_sector.t <- function(y){
 by_sector.t(6)
 by_sector.t(8)
 
+t.test(as.numeric(ap10[8,]), as.numeric(ap17[8,]), paired = TRUE) # Tucson
+
 #march april may 2010/2017
 
 by.month.t <- function(x){
@@ -72,5 +126,10 @@ by.month.t <- function(x){
   
 }
 
+#total numbers
 
+sum(ap17)
+sum(ap10)
+
+1 - sum(ap17)/sum(ap10)
 
